5c8a9081b50700ecf43bedcd2f8d0e31bcea4574
TalAizikov <31697771+TalAizikov@users.noreply.github.com>
2017-09-19 11:09:29 -0400

    Create Challenge: Favorite Fruits.js

---
+++ Challenge: Favorite Fruits.js
@@ -0,0 +1,6 @@
+var favFruit = ["Banana", "Watermelon", "Cherrys"];
+fill(255, 0, 0);
+text ("My top " + favFruit.length + " Favorite fruits", 10, 50);
+text(favFruit[0],135,50);
+text(favFruit[1],130,70);
+text(favFruit[2],134,27);
